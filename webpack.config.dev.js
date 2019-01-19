b82565e01e6fef4b4ae2ba31327ccf25cd024118
doYoungAn <jispoop@gmail.com>
2019-01-19 16:48:25 +0900

    webpack-dev-server detail config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,7 +6,10 @@ module.exports = merge(baseConfig, {
     mode: 'development',
     devServer: {
         port: 8080,
-        host: '0.0.0.0'
+        host: '0.0.0.0',
+        port: 8080,
+        historyApiFallBack: true,
+        publicPath: '/'
     },
     plugins: [
         new HtmlWebpackPlugin({
