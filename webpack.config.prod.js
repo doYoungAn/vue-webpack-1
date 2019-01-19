38d8144233126ec3c886c7bcdf5714ac46f9609a
doYoungAn <jispoop@gmail.com>
2019-01-19 16:38:38 +0900

    Create webpack.config.prod.js

---
+++ webpack.config.prod.js
@@ -0,0 +1,22 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(baseConfig, {
+    output: {
+        filename: '[name].[hash].js',
+        chunkFilename: '[name].[hash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './public/index.html',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseInlineTagWhitespace: true,
+                collapseWhitespace: true
+            }
+        })
+    ]
+});
