fef65c2c6c13eb1edfdd99123799be89171a4d17
doYoungAn <jispoop@gmail.com>
2019-01-19 16:59:43 +0900

    Create router.js

---
+++ src/router.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+const router = new Router({
+    mode: 'history',
+    routes: [
+
+    ]
+});
+
+export default router;
