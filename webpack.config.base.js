1a2a0f98d3361a77d9533ba8f22e0d9245eb67d9
doYoungAn <jispoop@gmail.com>
2019-01-19 16:18:41 +0900

    Create webpack.config.base.js

---
+++ webpack.config.base.js
@@ -0,0 +1,31 @@
+const { VueLoaderPlugin } = require('vue-router')
+
+module.exports = {
+    entry: {
+        main: './main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: 'url-loader',
+                options: {
+                limit: 100000,
+                name: '[name].[ext]',
+                outputPath: '/assets/fonts'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
